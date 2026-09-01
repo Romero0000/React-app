c8a5ff29f07f22658b295604c16aca16486be4c7
Romero0000 <lolkekazaza2283221337@mail.ru>
2026-09-01 15:11:50 +0300

    Pages & cards

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import { Button } from "../Button";
 import cls from "./Header.module.css"
 import ReactLogo from "../../../src/assets/icons8-react-native-64.png"
+import { useNavigate } from "react-router-dom";
 
 export const Header = () =>{
+    const navigate = useNavigate();
     return(
         <header className={cls.header}>
-            <p>
+            <p onClick={()=> navigate("/")}>
                 <img src={ReactLogo} />
                 <span>React Cards</span>
             </p>
             <div className={cls.headerButtons}>
-                <Button>
+                <Button onClick={()=>navigate("addquestion")}>
                     Add card
                 </Button>
                 <Button>
