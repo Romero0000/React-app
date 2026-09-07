e70c1e3658337af81335c1bd3f2a4955407dd9c9
Romero0000 <lolkekazaza2283221337@mail.ru>
2026-09-07 18:14:37 +0300

    Not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { MainLayout } from './components/MainLayout';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
       <Route path="/" element={<HomePage/>}/>
       <Route path="/forbidden" element={<div>Forbidden..</div>}/>
       <Route path="/addquestion" element={<div>Add question</div>}/>
+      <Route path="/question/:id" element={<div>question page</div>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
     </Route>
   </Routes>
   </BrowserRouter>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,9 @@ import { QuestionCard } from "../../components/QuestionCard";
 import cls from "./HomePage.module.css";
 
 export const HomePage = ()=>{
-    return <div> HomePage 
-
-        <QuestionCard/>
-    </div>
+    return 
+        <>
+        
+        {card}
+        </> 
 }
