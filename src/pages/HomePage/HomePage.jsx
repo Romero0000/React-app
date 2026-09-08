47dad70128deff150193796b8eade657add41d60
Romero0000 <lolkekazaza2283221337@mail.ru>
2026-09-08 13:14:30 +0300

    useEffect, move db

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 import { QuestionCard } from "../../components/QuestionCard";
+import { API_URL } from "../../constans";
 import cls from "./HomePage.module.css";
 
 export const HomePage = ()=>{
-    return 
+
+    const [cards, setCards] = useState([]);
+
+    const getQuestions = async()=>{
+        try{
+            const response = await fetch(`${API_URL}/react`);
+            const questions = await response.json();
+            setCards(questions);
+
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    useEffect(() =>{
+        getQuestions();
+    }, [])
+    
+    return (
         <>
-        
-        {card}
-        </> 
+        {cards.map((card,index)=> {
+            return <QuestionCard card={card} key={index}/>
+        })}
+       
+        </> );
 }
